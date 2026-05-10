47fb00f9c8c6da3c4a130fda1650e91ff39840bd
neerajg-03 <gupta.neeraj9000@gmail.com>
2026-05-10 15:55:53 +0530

    Delete server/routes directory

--- server/routes/services_temp.js
+++
@@ -1,7 +0,0 @@
-// Fixed query section
-    // Build query for admin services with provider requests
-    const query = {
-      provider: null, // Admin-created services only
-      'providerRequests.isApproved': false,
-      'providerRequests.isRejected': { $ne: true }
-    };

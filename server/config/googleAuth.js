0268a7a190a4d324fecd24bfb90eddc501d7a262
neerajg-03 <gupta.neeraj9000@gmail.com>
2026-05-14 00:18:23 +0530

    Update googleAuth.js

--- server/config/googleAuth.js
+++ server/config/googleAuth.js
@@ -30,7 +30,7 @@ if (
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL:
-          'http://localhost:5000/api/auth/google/callback'
+          'https://insta-serve-1-0.onrender.com/api/auth/google/callback'
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
